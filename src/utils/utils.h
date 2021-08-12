83cf65bd7e2a80e06b7926c2ed114f19beef663c
Anton Adamansky <adamansky@gmail.com>
2021-08-12 16:58:42 +0700

    *

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -36,32 +36,3 @@ static iwrc utils_ts(int64_t *out) {
   }
 }
 
-static char* utils_bin2hex(
-  char* const                hex,
-  const size_t               hex_maxlen,
-  const unsigned char* const bin,
-  const size_t               bin_len) {
-
-  size_t i = (size_t) 0U;
-  unsigned int x;
-  int b;
-  int c;
-
-  if ((bin_len >= SIZE_MAX / 2) || (hex_maxlen <= bin_len * 2U)) {
-    //errx(2, "bin2hex length wrong");
-    return 0;
-  }
-  while (i < bin_len) {
-    c = bin[i] & 0xf;
-    b = bin[i] >> 4;
-    x = (unsigned char) (87U + c + (((c - 10U) >> 8) & ~38U)) << 8
-        | (unsigned char) (87U + b + (((b - 10U) >> 8) & ~38U));
-    hex[i * 2U] = (char) x;
-    x >>= 8;
-    hex[i * 2U + 1U] = (char) x;
-    i++;
-  }
-  hex[i * 2U] = 0U;
-  return hex;
-}
-
