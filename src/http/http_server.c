7a83883d53521dd9afbcd0f768b36a53e3c32d31
Anton Adamansky <adamansky@gmail.com>
2021-12-02 23:32:37 +0700

    *

--- src/http/http_server.c
+++ src/http/http_server.c
@@ -1,13 +1,36 @@
+#include <iowow/iwlog.h>
 #include "http_server.h"
 #include "poller_adapter.h"
 
-iwrc iwn_http_server_create(
-  struct iwn_poller *p, int port, const char *listen, bool http,
-  iwn_http_server_handle_t *out_handle) {
+#include <sys/epoll.h>
 
+static int64_t _on_poller_adapter_event(struct iwn_poller_adapter *pa, void *user_data, uint32_t events) {
   return 0;
 }
 
-iwrc iwn_http_server_dispose(iwn_http_server_handle_t h) {
-  return 0;
+static void _on_poller_adapter_dispose(struct iwn_poller_adapter *pa, void *user_data) {
+}
+
+iwrc iwn_http_server_create(const struct iwn_http_server_spec *spec_, iwn_http_server_fd_t *out_fd) {
+  iwrc rc = 0;
+  struct iwn_http_server_spec spec = *spec_;
+
+  if (!spec.poller) {
+    return IW_ERROR_INVALID_ARGS;
+  }
+  if (!spec.listen) {
+    spec.listen = "localhost";
+  }
+  if (!spec.port) {
+    spec.port = spec.https ? 8443 : 8080;
+  }
+
+
+  return rc;
+}
+
+iwrc iwn_http_server_request_dispose(iwn_http_server_fd_t h) {
+  iwrc rc = 0;
+
+  return rc;
 }
--- src/http/http_server.h
+++ src/http/http_server.h
@@ -1,10 +1,35 @@
 #pragma once
 #include "poller.h"
 
-typedef intptr_t iwn_http_server_handle_t;
+typedef int iwn_http_server_fd_t;
 
-iwrc iwn_http_server_create(
-  struct iwn_poller *p, int port, const char *listen, bool http,
-  iwn_http_server_handle_t *out_handle);
+struct iwn_http_server {
+  const char *listen;
+  void       *user_data;
+  iwn_http_server_fd_t fd;
+  int port;
+};
 
-iwrc iwn_http_server_dispose(iwn_http_server_handle_t h);
+struct iwn_http_server_connection {
+  const struct iwn_http_server *server;
+  int fd;
+};
+
+typedef void (*iwn_http_server_on_dispose)(const struct iwn_http_server *server);
+
+typedef void (*iwn_http_server_on_connection)(const struct iwn_http_server_connection *conn);
+
+struct iwn_http_server_spec {
+  const struct iwn_poller *poller;
+  const char *listen;
+  void       *user_data;
+  iwn_http_server_on_connection on_connection;
+  iwn_http_server_on_dispose    on_dispose;
+  long connection_timeout_sec;
+  int  port;
+  bool https;
+};
+
+iwrc iwn_http_server_create(const struct iwn_http_server_spec *spec, iwn_http_server_fd_t *out_fd);
+
+iwrc iwn_http_server_request_dispose(iwn_http_server_fd_t h);
