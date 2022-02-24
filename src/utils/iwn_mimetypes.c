1fe59a2161c439190f5beaba809065d9b1b6e721
Anton Adamansky <adamansky@gmail.com>
2022-02-24 11:36:26 +0700

    * Added iwn_mimetype_find_by_path() (iwn_mimetypes.h)

--- src/utils/iwn_mimetypes.c
+++ src/utils/iwn_mimetypes.c
@@ -4,3 +4,16 @@
 const char* iwn_mimetype_find(const char *ext) {
   return _mimetype_find(ext);
 }
+
+const char* iwn_mimetype_find_by_path(const char *path) {
+  int len = (int) strlen(path);
+  if (len < 2 || path[len - 1] == '.') {
+    return 0;
+  }
+  for (int i = len - 2; i >= 0; --i) {
+    if (path[i] == '.') {
+      return _mimetype_find(path + i + 1);
+    }
+  }
+  return 0;
+}
--- src/utils/iwn_mimetypes.h
+++ src/utils/iwn_mimetypes.h
@@ -6,4 +6,6 @@ IW_EXTERN_C_START
 
 IW_EXPORT const char* iwn_mimetype_find(const char *ext);
 
+IW_EXPORT const char* iwn_mimetype_find_by_path(const char *path);
+
 IW_EXTERN_C_END
