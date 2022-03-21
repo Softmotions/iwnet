db3b2b5cbc5868c9f2353949667c0cf72b9d812b
Anton Adamansky <adamansky@gmail.com>
2022-03-22 00:30:49 +0700

    * Fixed iwn_mimetype_find_by_path2()

--- src/utils/iwn_mimetypes.c
+++ src/utils/iwn_mimetypes.c
@@ -1,17 +1,22 @@
 #include "iwn_mimetypes.h"
 #include "generated_mimegen.h"
 
+#include <limits.h>
+
 const char* iwn_mimetype_find(const char *ext) {
   return _mimetype_find(ext);
 }
 
 const char* iwn_mimetype_find_by_path2(const char *path, size_t len) {
-  if (len < 2 || path[len - 1] == '.') {
+  if (len < 2 || path[len - 1] == '.' || len > PATH_MAX) {
     return 0;
   }
   for (int i = (int) len - 2; i >= 0; --i) {
     if (path[i] == '.') {
-      return _mimetype_find(path + i + 1);
+      char buf[len - i];
+      memcpy(buf, path + i + 1, len - i - 1);
+      buf[len - i - 1] = '\0';
+      return _mimetype_find(buf);
     }
   }
   return 0;
