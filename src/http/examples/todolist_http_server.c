bf5bfe9b9bb981e67bcbd24c5f20de67027c2a28
Anton Adamansky <adamansky@gmail.com>
2022-04-14 23:39:31 +0700

    * Finished todo list example

--- src/http/examples/todolist_http_server.c
+++ src/http/examples/todolist_http_server.c
@@ -16,7 +16,7 @@ static struct iwn_wf_ctx *ctx;
 struct item {
   int   id;
   char *title;
-  char *body;
+  char *brief;
   bool  done;
   struct item *next;
 };
@@ -31,7 +31,7 @@ static void _on_signal(int signo) {
 static void _item_destroy(struct item *item) {
   if (item) {
     free(item->title);
-    free(item->body);
+    free(item->brief);
     free(item);
   }
 }
@@ -55,6 +55,29 @@ static int _item_add(struct item *a) {
   return ++a->id;
 }
 
+static void _item_remove(int id) {
+  for (struct item *n = items, *p = 0; n; p = n, n = n->next) {
+    if (n->id == id) {
+      if (p) {
+        p->next = n->next;
+      } else {
+        items = n->next;
+      }
+      _item_destroy(n);
+      break;
+    }
+  }
+}
+
+static struct item* _item_get(int id) {
+  for (struct item *n = items; n; n = n->next) {
+    if (n->id == id) {
+      return n;
+    }
+  }
+  return 0;
+}
+
 /// Todo list create item.
 ///
 /// * curl -XPUT -d'Say Hello' http://localhost:8080/todo/Hello
@@ -72,9 +95,9 @@ static int _todo_create(struct iwn_wf_req *req, void *user_data) {
   item->title[m->ep - m->sp] = '\0';
 
   if (req->body_len) {
-    RCB(finish, item->body = strndup(req->body, req->body_len));
+    RCB(finish, item->brief = strndup(req->body, req->body_len));
   } else {
-    RCB(finish, item->body = strdup(""));
+    RCB(finish, item->brief = strdup(""));
   }
 
   // Get done flag from query parameter:
@@ -85,7 +108,7 @@ static int _todo_create(struct iwn_wf_req *req, void *user_data) {
   _item_add(item);
 
   // Write response to the client
-  if (iwn_http_response_printf(req->http, 200, "text/plain", "%d\n", item->id)) {
+  if (iwn_http_response_printf(req->http, 201, "text/plain", "%d\n", item->id)) {
     ret = IWN_WF_RES_PROCESSED;
   }
 
@@ -108,10 +131,10 @@ static int _todo_list(struct iwn_wf_req *req, void *user_data) {
 
   // For the sake of simplicity response is not streamed and assembled as buffer.
   RCB(finish, xstr = iwxstr_new());
-  
-  iwxstr_printf(xstr, "#   Done\tTitle\tDescription\n");
+
+  iwxstr_printf(xstr, "#  \tDone\tTitle\n");
   for (struct item *n = items; n; n = n->next) {
-    iwxstr_printf(xstr, "%03d %s\t%s\t%s\n", n->id, (n->done ? "done" : ""), n->title, n->body);
+    iwxstr_printf(xstr, "%03d\t%s\t%s\n", n->id, (n->done ? "done" : " "), n->title);
   }
 
   if (iwn_http_response_write(req->http, 200, "text/plain", iwxstr_ptr(xstr), iwxstr_size(xstr))) {
@@ -123,15 +146,83 @@ finish:
   if (rc) {
     iwlog_ecode_error3(rc);
   }
-  return 0;
+  return ret;
 }
 
+/// Get todo item by id.
+///
+/// * curl http://localhost:8080/todo/2
+///
 static int _todo_get(struct iwn_wf_req *req, void *user_data) {
-  return 0;
+  iwrc rc = 0;
+  IWXSTR *xstr;
+  int ret = IWN_WF_RES_INTERNAL_ERROR;
+
+  struct iwn_wf_route_submatch *m = iwn_wf_request_submatch_first(req);
+  int id = iwatoi2(m->sp, m->ep - m->sp);
+  struct item *n = _item_get(id);
+  if (!n) {
+    return 404;
+  }
+
+  RCB(finish, xstr = iwxstr_new());
+  RCC(rc, finish, iwxstr_printf(xstr, "ID:\t%d\n", n->id));
+  RCC(rc, finish, iwxstr_printf(xstr, "Title:\t%s\n", n->title));
+  RCC(rc, finish, iwxstr_printf(xstr, "Done:\t%s\n", n->done ? "yes" : "no"));
+  RCC(rc, finish, iwxstr_printf(xstr, "Brief:\t%s\n", n->brief));
+
+  if (iwn_http_response_write(req->http, 200, "text/plain", iwxstr_ptr(xstr), iwxstr_size(xstr))) {
+    ret = IWN_WF_RES_PROCESSED;
+  }
+
+finish:
+  iwxstr_destroy(xstr);
+  if (rc) {
+    iwlog_ecode_error3(rc);
+  }
+  return ret;
 }
 
+/// Remove todo item with id.
+///
+/// * curl -XDELETE http://localhost:8080/todo/2
+///
 static int _todo_remove(struct iwn_wf_req *req, void *user_data) {
-  return 0;
+  struct iwn_wf_route_submatch *m = iwn_wf_request_submatch_first(req);
+  int id = iwatoi2(m->sp, m->ep - m->sp);
+  struct item *n = _item_get(id);
+  if (!n) {
+    return 404;
+  }
+  _item_remove(id);
+  return 204; // No content
+}
+
+/// Update todo item `done` status.
+///
+/// * curl -XPOST -d'id=2&done=1' http://localhost:8080/todo
+///
+static int _todo_done(struct iwn_wf_req *req, void *user_data) {
+  int id;
+  bool done;
+
+  struct iwn_val val = iwn_pair_find_val(&req->form_params, "id", IW_LLEN("id"));
+  if (!val.len) {
+    return 400;
+  }
+  id = iwatoi2(val.buf, val.len);
+  struct item *n = _item_get(id);
+  if (!n) {
+    return 404;    
+  }
+  val = iwn_pair_find_val(&req->form_params, "done", IW_LLEN("done"));
+  if (val.len) {
+    n->done = strcmp(val.buf, "1") == 0;
+  } else {
+    n->done = !n->done;
+  }
+
+  return 200;
 }
 
 int main(int argc, char *argv[]) {
@@ -199,6 +290,14 @@ int main(int argc, char *argv[]) {
     .tag = "list"
   }, 0));
 
+  // Update task status
+  RCC(rc, finish, iwn_wf_route(&(struct iwn_wf_route) {
+    .parent = parent,
+    .handler = _todo_done,
+    .flags = IWN_WF_POST,
+    .tag = "done"
+  }, 0));
+
   RCC(rc, finish, iwn_poller_create(0, 0, &poller));
 
   struct iwn_wf_server_spec spec = {
@@ -217,6 +316,26 @@ int main(int argc, char *argv[]) {
   iwn_wf_route_print(ctx->root, stderr);
   // Configure HTTP server.
   RCC(rc, finish, iwn_wf_server(&spec, ctx));
+
+  {
+    const char *proto = ssl ? "https" : "http";
+    fprintf(stderr, "\n Create a new 'Hello' todo entry:\n");
+    fprintf(stderr, "\tcurl -k -XPUT -d'Say Hello' %s://%s:%d/todo/Hello\n", proto, spec.listen, spec.port);
+    fprintf(stderr, "\tcurl -k -XPUT -d'Say Hello' %s://%s:%d/todo/Hello?done=1\n", proto, spec.listen, spec.port);
+
+    fprintf(stderr, "\n List all todo list items:\n");
+    fprintf(stderr, "\tcurl -k %s://%s:%d/todo\n", proto, spec.listen, spec.port);
+
+    fprintf(stderr, "\n Get task #1 details:\n");
+    fprintf(stderr, "\tcurl -k %s://%s:%d/todo/1\n", proto, spec.listen, spec.port);
+
+    fprintf(stderr, "\n Remove task #2 from todo list:\n");
+    fprintf(stderr, "\tcurl -k -XDELETE %s://%s:%d/todo/2\n", proto, spec.listen, spec.port);
+
+    fprintf(stderr, "\n Update done status of task #2:\n");
+    fprintf(stderr, "\tcurl -k -XPOST -d'id=2&done=1' %s://%s:%d/todo\n", proto, spec.listen, spec.port);
+  }
+
   // Start fds poller reactor.
   iwn_poller_poll(poller);
 
